13338f1314d7de878c5c9800e3cb59b122f1716a
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2019-02-06 14:28:42 +0530

    on Click Render

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, {Component, Fragment} from 'react'
 import Link from 'next/link';
 
-const HomePage = props => {
-    return (
-        <div>
-            Hello, World!
-            <br/>
-            <Link href='/stars'><a>Stars</a></Link>
-        </div>
-    );
+class HomePage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {stars: null, visible: false}
+    }
+
+    handleClick = async () => {
+        const res = await fetch('https://api.github.com/repos/v4iv/gatsby-starter-business')
+        const json = await res.json()
+        this.setState({stars: json.stargazers_count, visible: true})
+    }
+
+    render() {
+        return (
+            <Fragment>
+                Hello, World!
+                <br/>
+                <Link href='/stars'><a>Stars</a></Link>
+                <br/>
+                <br/>
+                <button onClick={this.handleClick}>Click Me</button>
+                <br/>
+                <br/>
+                {this.state.visible &&
+                <div>Stars: {this.state.stars}</div>
+                }
+            </Fragment>
+        );
+    }
 }
 
 export default HomePage
